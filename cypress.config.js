89d8d49de10e98b44feef08630bf7f1a96e994c8
Micah Wood <micah@wpscholar.com>
2023-05-30 17:30:59 -0400

    Clean up cypress.config.js and prep for test matrix

--- cypress.config.js
+++ cypress.config.js
@@ -1,32 +1,77 @@
-const { defineConfig } = require('cypress')
+const {defineConfig} = require('cypress')
+const cypressReplay = require("@replayio/cypress")
+const {phpVersion, core} = require('./.wp-env.json')
+const wpVersion = /[^/]*$/.exec(core)[0]
 
 module.exports = defineConfig({
-  env: {
-    wpUsername: 'admin',
-    wpPassword: 'password',
-  },
-  fixturesFolder: 'tests/cypress/fixtures',
-  screenshotsFolder: 'tests/cypress/screenshots',
-  video: false,
-  videosFolder: 'tests/cypress/videos',
-  videoUploadOnPasses: false,
-  chromeWebSecurity: false,
-  viewportWidth: 1024,
-	viewportHeight: 768,
-	blockHosts: [
-		'*doubleclick.net',
-		'*jnn-pa.googleapis.com',
-		'*youtube.com',
-	],
-  e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./tests/cypress/plugins/index.js')(on, config)
+    env: {
+        wpUsername: 'admin',
+        wpPassword: 'password',
+        wpVersion,
+        phpVersion,
     },
-    baseUrl: 'http://localhost:8880',
-    specPattern: 'tests/cypress/integration/**/*.cy.{js,jsx,ts,tsx}',
-    supportFile: 'tests/cypress/support/index.js',
-    testIsolation: false,
-  },
+    downloadsFolder: 'tests/cypress/downloads',
+    fixturesFolder: 'tests/cypress/fixtures',
+    screenshotsFolder: 'tests/cypress/screenshots',
+    video: false,
+    videosFolder: 'tests/cypress/videos',
+    videoUploadOnPasses: false,
+    chromeWebSecurity: false,
+    viewportWidth: 1024,
+    viewportHeight: 768,
+    blockHosts: [
+        '*doubleclick.net',
+        '*jnn-pa.googleapis.com',
+        '*youtube.com',
+    ],
+    e2e: {
+        setupNodeEvents(on, config) {
+
+            const semver = require('semver');
+
+            // Setup Replay
+            cypressReplay.default(on, config);
+
+            // Ensure that the base URL is always properly set.
+            if (config.env && config.env.baseUrl) {
+                config.baseUrl = config.env.baseUrl;
+            }
+
+            // Ensure that we have a semantically correct WordPress version number for comparisons.
+            if (config.env.wpVersion) {
+                if (config.env.wpVersion.split('.').length !== 3) {
+                    config.env.wpSemverVersion = `${config.env.wpVersion}.0`;
+                } else {
+                    config.env.wpSemverVersion = config.env.wpVersion;
+                }
+            }
+
+            if (config.env.phpVersion) {
+                if (config.env.phpVersion.split('.').length !== 3) {
+                    config.env.phpSemverVersion = `${config.env.phpVersion}.0`;
+                } else {
+                    config.env.phpSemverVersion = config.env.phpVersion;
+                }
+            }
+
+            on('task', {
+                log(message) {
+                    console.log(message)
+
+                    return null
+                },
+                table(message) {
+                    console.table(message)
+
+                    return null
+                }
+            })
+
+            return config;
+        },
+        baseUrl: 'http://localhost:8884',
+        specPattern: 'tests/cypress/integration/**/*.cy.{js,jsx,ts,tsx}',
+        supportFile: 'tests/cypress/support/index.js',
+        testIsolation: false,
+    },
 })
