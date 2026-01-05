60ba3de130177927b9f4ace9dc5bd93d63800cfd
Ramya krishna <ramyakrishnaimminni@gmail.com>
2026-01-05 16:20:26 +0530

    changes

--- inc/Admin.php
+++ inc/Admin.php
@@ -22,7 +22,7 @@ final class Admin {
 		/* Add Page to WordPress Admin Menu. */
 		\add_action( 'admin_menu', array( __CLASS__, 'page' ) );
 		/* Load Page Scripts & Styles. */
-		\add_action( 'load-toplevel_page_crazy-domains', array( __CLASS__, 'assets' ) );
+		\add_action( 'admin_enqueue_scripts', array( __CLASS__, 'assets' ) );
 		/* Add Links to WordPress Plugins list item. */
 		\add_filter( 'plugin_action_links_wp-plugin-crazy-domains/wp-plugin-crazy-domains.php', array( __CLASS__, 'actions' ) );
 		/* Add inline style to hide subnav link */
