6fdf407a65e256053930d22dbcaae4d014e7e99b
Ramya Krishna <ramya.krishna@newfold.com>
2026-09-24 12:16:37 +0530

    tested upto header updated with respect of wp-env.json

--- wp-plugin-crazy-domains.php
+++ wp-plugin-crazy-domains.php
@@ -15,7 +15,7 @@
  * Version:           2.5.5
  * Requires PHP:      7.3
  * Requires at least: 6.6
- * Tested up to:      7.1
+ * Tested up to:      7.1.2
  * Author:            Crazy Domains
  * Author URI:        https://crazydomains.com
  * Text Domain:       wp-plugin-crazy-domains
